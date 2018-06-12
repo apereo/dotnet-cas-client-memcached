e7c44d0468e8864865c2b8045e9e7f016fc9c7b8
Jason Kanaris <phantomtypist@hotmail.com>
2018-06-12 12:13:53 -0400

    - Added nuspec file for DotNetCasClient.Memcached nuget package. - Setup Cake build script.  Fixed Cake to version 0.21.0 for now.  There were problems with newer versions and the current script.

--- src/DotNetCasClient.Memcached/Properties/AutoGeneratedAssemblyInfo.cs
+++ src/DotNetCasClient.Memcached/Properties/AutoGeneratedAssemblyInfo.cs
@@ -6,11 +6,11 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("Apereo .NET CAS Client Memcached backed Proxy/Service Ticket Managers")]
-[assembly: AssemblyDescription("Memcached backed Proxy/Service Ticket Managers for the Apereo .NET CAS Client")]
+[assembly: AssemblyDescription("Memcached backed Proxy/Service Ticket Managers for the Apereo .NET CAS Client.")]
 [assembly: AssemblyCompany("Apereo Foundation")]
 [assembly: AssemblyProduct("Apereo .NET CAS Client Memcached backed Proxy/Service Ticket Managers")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-unstable.9+Branch.develop.Sha.62490d364967bd45f692f91e8cbce831795a8f33")]
 [assembly: AssemblyCopyright("Copyright (c) 2007-2018 Apereo.  All rights reserved.")]
 
