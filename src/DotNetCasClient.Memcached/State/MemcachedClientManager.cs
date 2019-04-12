a28ba2e41721b4bccfbbc99ae618e61eab084191
Jason Kanaris <phantomtypist@hotmail.com>
2019-04-12 02:51:43 -0400

    Finished implementing the Memcached-backed ticket managers.

---
+++ src/DotNetCasClient.Memcached/State/MemcachedClientManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Enyim.Caching;
+
+namespace DotNetCasClient.State
+{
+    /// <summary>
+    /// Represents a singleton implementation of the MemcachedClient.
+    /// </summary>
+    public class MemcachedClientManager
+    {
+        private static readonly Lazy<MemcachedClient> lazy = new Lazy<MemcachedClient>(() => new MemcachedClient());
+
+        /// <summary>
+        /// Gets the instance of MemcachedClient.
+        /// </summary>
+        public static MemcachedClient Instance { get { return lazy.Value; } }
+
+        private MemcachedClientManager()
+        {
+        }
+    }
+}
